95aefa2812d9175798480162874dc06ab60db6b8
Alex Robinson <robinsal@umd.edu>
2019-09-29 12:38:34 -0400

    resized grid, added hover functionality to cells

--- domman.js
+++ domman.js
@@ -1,32 +1,38 @@
 const body = document.querySelector('body');
+const container = document.createElement('div');
+container.style.top = '15px';
+container.style.left = '320px';
+container.style.position = 'absolute';
+container.style.visibility = 'show';
+body.appendChild(container);
 
 // Create a 2D array of 16 div nodes
 var grid = []
 var i;
 var j;
-for (i = 0; i <= 15; i++) {
+for (i = 0; i < 30; i++) {
     grid[i] = []
     const row = document.createElement('div');
     row.style.display = 'inline-block';
-    body.appendChild(row);
+    container.appendChild(row);
 
-    for (j = 0; j <= 15; j++) {
+    for (j = 0; j < 30; j++) {
         grid[i].push(document.createElement('div'));
         curr = grid[i][j];
+        curr.class = 'cell'
         curr.style.border = '1px solid';
-        curr.style.width = '50px';
-        curr.style.height = '50px';
+        curr.style.width = '20px';
+        curr.style.height = '20px';
         curr.style.backgroundColor = 'white';
-        function hoverColor(ele) {
-            if (ele.style.backgroundColor === 'white') {
-                ele.style.backgroundColor = 'black';
-            } else {
-                ele.style.backgroundColor = 'white';
-            }
-        }
-        //curr.addEventListener('mouseover', hoverColor(curr));
 
         row.appendChild(curr);
+
+        function setColor() {
+            this.style.backgroundColor === 'white' ?
+                this.style.backgroundColor = 'black' :
+                this.style.backgroundColor = 'white';
+        }
+        curr.onmouseover = setColor;
+        console.log(curr);
     }
 }
-
