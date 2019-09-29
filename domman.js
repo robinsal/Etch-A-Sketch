b174cde87eb7a162b72cb053f287f77499d65d94
Alex Robinson <robinsal@umd.edu>
2019-09-29 11:38:37 -0400

    grid displays correctly

--- domman.js
+++ domman.js
@@ -1,5 +1,4 @@
-const container = document.querySelector('body');
-console.log(container);
+const body = document.querySelector('body');
 
 // Create a 2D array of 16 div nodes
 var grid = []
@@ -7,10 +6,27 @@ var i;
 var j;
 for (i = 0; i <= 15; i++) {
     grid[i] = []
+    const row = document.createElement('div');
+    row.style.display = 'inline-block';
+    body.appendChild(row);
+
     for (j = 0; j <= 15; j++) {
-        grid[i].push(document.createElement('div'))
-        //grid[i][j].
-        container.appendChild(grid[i][j]);
+        grid[i].push(document.createElement('div'));
+        curr = grid[i][j];
+        curr.style.border = '1px solid';
+        curr.style.width = '50px';
+        curr.style.height = '50px';
+        curr.style.backgroundColor = 'white';
+        function hoverColor(ele) {
+            if (ele.style.backgroundColor === 'white') {
+                ele.style.backgroundColor = 'black';
+            } else {
+                ele.style.backgroundColor = 'white';
+            }
+        }
+        //curr.addEventListener('mouseover', hoverColor(curr));
+
+        row.appendChild(curr);
     }
 }
 
